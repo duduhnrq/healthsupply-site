a27751aaa81f44d2a61a62e19e51867629bef9df
Eduardo Henrique <dudu30rafa@gmail.com>
2025-02-18 10:37:32 -0300

    novo projeto

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const menu = document.getElementById("menu");
+  
+    function checkScreenSize() {
+      if (window.innerWidth > 768) {
+        menu.classList.remove("active");
+        menu.style.display = "flex"; 
+        menu.style.opacity = "1";
+        menu.style.visibility = "visible";
+        menu.style.transform = "none";
+      } else {
+        menu.style.display = "";
+        menu.style.opacity = "";
+        menu.style.visibility = "";
+        menu.style.transform = "";
+      }
+    }
+  
+    menuToggle.addEventListener("click", function () {
+      menu.classList.toggle("active");
+    });
+  
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+  });
